534f501d9b56ab453aa76a96ba5f0270ce4f3501
Meredith Bashert <merbashert@gmail.com>
2021-06-02 13:42:22 -0400

    Built base page

--- spice-tracker/src/App.js
+++ spice-tracker/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = props => {
+
+    let baseUrl = '';
+    if (process.env.NODE_ENV === 'development') {
+        baseUrl = 'http://localhost:8888'
+    } else {
+        baseUrl = 'https://bingetracker.herokuapp.com'
+    }
+
+    const[savedSpices, setSavedSpices] = useState([])
+
+
+    const fetchSpices = () => {
+        fetch(`${baseUrl}/spices`)
+        .then(data => data.json())
+        .then(jData => {
+            setSavedSpices(jData)
+        }).catch(err=>console.log(err))
+    }
+
+    useEffect(() => {
+        fetchSpices()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [savedSpices])
+
+    return (
+        <div>
+
+            {savedSpices.map((savedSpice, index) => {
+                return (
+                    <div>
+            
+
+                    {savedSpice.name}{savedSpice.category}
+
+                </div>
+            )
+        })}
     </div>
-  );
+)
 }
 
-export default App;
+
+export default App
