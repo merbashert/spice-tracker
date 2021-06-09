81103023c39612ad99736c4ba61c742c49fae34f
Meredith Bashert <merbashert@gmail.com>
2021-06-09 15:05:40 -0400

    Added delete button to spice

--- spice-tracker/src/Components/Spice.js
+++ spice-tracker/src/Components/Spice.js
@@ -1,5 +1,6 @@
 
 import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
 
 
 const Spice = props => {
@@ -7,13 +8,13 @@ const Spice = props => {
 
 
     return (
-        <>
+        <tr>
         <td className='spice-name'>{props.savedSpice.name}</td>
         <td className='spice-details'>{props.savedSpice.category}</td>
         <td className = 'spice-box'>{props.savedSpice.date_purchased}</td>
-        <td className='spice-buttons'></td>
-
-        </>
+        <td className='spice-buttons'>
+            <Button onClick={(e) => {if (window.confirm(`Definitely delete ${props.savedSpice.name}?`)) props.handleDeleteSpice(props.savedSpice.id)}}>Delete</Button></td>
+        </tr>
 
     )
 }
