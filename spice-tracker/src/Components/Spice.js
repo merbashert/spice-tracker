a2b271040d8fe19f146020fe0abdeaa21b9695b8
Meredith Bashert <merbashert@gmail.com>
2023-07-01 17:28:06 -0400

    Fixed date formatting

--- spice-tracker/src/Components/Spice.js
+++ spice-tracker/src/Components/Spice.js
@@ -10,7 +10,7 @@ const Spice = props => {
     const [show, setShow] = useState(false);
 
     const today = new Date(),
-   date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+   date = today.getFullYear() + '-' + (today.getMonth < 10 ? (today.getMonth() + 1):'0'+today.getMonth()) + '-' + today.getDate();
 
 
     const handleClose = () => {
@@ -19,7 +19,10 @@ const Spice = props => {
 
     const handleShow = () => {
         setShow(true)
+        console.log(date);
     }
+
+    const shortened_expiration_date = props.savedSpice.expiration_date.slice(0, -3);
 
     const handleUpdateSpice = (updateData) => {
         fetch(`${props.baseUrl}/spices/${props.savedSpice.id}`, {
@@ -47,10 +50,9 @@ const Spice = props => {
 
         <tr>
             <td className='spice-name'>{props.savedSpice.name}</td>
-            <td className='spice-category'>{props.savedSpice.category}</td>
 
-            {props.savedSpice.expiration_date < date ?
-                <td className = 'spice-date'>{props.savedSpice.expiration_date}</td> : <td className = 'past-spice-date'>{props.savedSpice.expiration_date}</td>}
+            {props.savedSpice.expiration_date > date ?
+                <td className = 'spice-date'>{shortened_expiration_date}</td> : <td className = 'past-spice-date'>{shortened_expiration_date}</td>}
 
             <td className='spice-buttons'>
                 <Button variant="outline-success" onClick={handleShow}>Edit</Button>
